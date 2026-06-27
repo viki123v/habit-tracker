96c77fd8122aec2a498de1873caa6e4097582419
viki123v <viktorhristovski629@gmail.com>
2026-06-27 09:38:00 +0200

    Remove temp widget

--- lib/src/routing/routes.dart
+++ lib/src/routing/routes.dart
@@ -3,7 +3,6 @@ import 'package:go_router/go_router.dart';
 import 'package:habit_tracker/src/ui/core/shared/footer/footer_view.dart';
 import 'package:habit_tracker/src/ui/core/shared/naviagtion_bar/navigation_bar_view.dart';
 import 'package:habit_tracker/src/ui/page_not_found/page_not_found_view.dart';
-import 'package:habit_tracker/src/ui/screens/check_theme/check_theme_view.dart';
 import 'package:habit_tracker/src/ui/screens/habit/creation/habit_creation.dart';
 import 'package:habit_tracker/src/ui/screens/habit/details/habit_details.dart';
 import 'package:habit_tracker/src/ui/screens/home/home_view.dart';
@@ -18,10 +17,9 @@ final router = GoRouter(
     GoRoute(
       path: "/",
       builder: (context, state) => Scaffold(
-        body: ThemePreviewView()
+        body: LoginView()
       ),
       routes: [
-        GoRoute(path: "login", builder: (_, _) => LoginView()),
         GoRoute(path: "home", builder: (_, _) => HomeView()),
         GoRoute(
           path: "habit",
