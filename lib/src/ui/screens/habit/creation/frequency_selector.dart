adeba0e94609e4cd95cc69f2551bf97d057dc37a
viki123v <viktorhristovski629@gmail.com>
2026-07-05 01:10:01 +0200

    Add animations for changes

--- lib/src/ui/screens/habit/creation/frequency_selector.dart
+++ lib/src/ui/screens/habit/creation/frequency_selector.dart
@@ -22,16 +22,26 @@ class FrequencySelector extends StatefulWidget {
 class _FrequencySelectorState extends State<FrequencySelector> {
   @override
   Widget build(BuildContext context) {
+    late final Widget child;
+
     if (MontlyWidget.option == widget._selectedFreq) {
       widget._dto.type = HabitType.monthly;
-      return MontlyWidget(dto: widget._dto);
+      child = MontlyWidget(key: const ValueKey('monthly'), dto: widget._dto);
     } else if (WeeklyWidget.option == widget._selectedFreq) {
       widget._dto.type = HabitType.weekly;
-      return WeeklyWidget(dto: widget._dto);
+      child = WeeklyWidget(key: const ValueKey('weekly'), dto: widget._dto);
+    } else {
+      widget._dto.type = HabitType.daily;
+      widget._dto.dates = [];
+      child = const SizedBox(key: ValueKey('daily'));
     }
 
-    widget._dto.type = HabitType.daily;
-    widget._dto.dates = [];
-    return Container();
+    return AnimatedSwitcher(
+      duration: const Duration(milliseconds: 300),
+      transitionBuilder: (child, animation) {
+        return FadeTransition(opacity: animation, child: child);
+      },
+      child: child,
+    );
   }
 }
