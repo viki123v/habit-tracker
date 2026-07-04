3b4a38e0f46c6aabde1c877d0eac07fe3347c523
viki123v <viktorhristovski629@gmail.com>
2026-07-05 00:43:16 +0200

    Make login page initial route

--- lib/src/routing/routes.dart
+++ lib/src/routing/routes.dart
@@ -7,6 +7,8 @@ import 'package:habit_tracker/src/ui/screens/habit/creation/habit_creation.dart'
 import 'package:habit_tracker/src/ui/screens/habit/details/habit_details.dart';
 import 'package:habit_tracker/src/ui/screens/home/home_view.dart';
 import 'package:habit_tracker/src/ui/screens/home/home_viewmodel.dart';
+import 'package:habit_tracker/src/ui/screens/login/login_view.dart';
+import 'package:habit_tracker/src/ui/screens/login/login_viewmodel.dart';
 import 'package:habit_tracker/src/ui/screens/marketplace/marketplace_view.dart';
 import 'package:habit_tracker/src/ui/screens/profile/prifle_view.dart';
 import 'package:habit_tracker/src/ui/screens/report/report_view.dart';
@@ -20,18 +22,21 @@ final router = GoRouter(
   routes: [
     GoRoute(
       path: "/",
-      // builder: (context, state) => ChangeNotifierProvider(
-      // create: (context) => LoginViewModel(context.read()),
-      // child: const LoginView(),
-      // ),
-      builder: (ctx, state) => ChangeNotifierProvider(
-        create: (ctx) => HomeViewmodel(
-          ctx.read<HabitRepository>(),
-          ctx.read<ActiveUserRepository>(),
-        ),
-        child: const HomeView(),
+      builder: (context, state) => ChangeNotifierProvider(
+        create: (context) => LoginViewModel(context.read()),
+        child: LoginView(),
       ),
       routes: [
+        GoRoute(
+          path: "home",
+          builder: (ctx, state) => ChangeNotifierProvider(
+            create: (ctx) => HomeViewmodel(
+              ctx.read<HabitRepository>(),
+              ctx.read<ActiveUserRepository>(),
+            ),
+            child: const HomeView(),
+          ),
+        ),
         GoRoute(
           path: "habit",
           routes: [
