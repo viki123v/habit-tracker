e1a89fdc7ecd6427ec8587fb63475ad288a780ab
viki123v <viktorhristovski629@gmail.com>
2026-07-04 18:17:04 +0200

    Adapt views to display things

--- lib/src/ui/screens/profile/prifle_view.dart
+++ lib/src/ui/screens/profile/prifle_view.dart
@@ -3,6 +3,10 @@ import 'package:flutter/material.dart';
 class ProfileView extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return Text("profile view");
+    return Scaffold(
+      body: Center(
+        child: Text("profile view"),
+      ),
+    );
   }
 }
